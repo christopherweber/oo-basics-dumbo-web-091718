3f21e1dbdbfb7a94620f724d8a45572ac05dce3e
github username <github email address>
2018-09-21 11:59:23 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+# require 'pry'
 
+class Book
 
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
+# binding.pry
+  def initialize(title)
+    @title = title
+    @page_count =page_count
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
